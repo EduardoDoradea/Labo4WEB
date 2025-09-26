0cfda97884a52767257e26c609256768dc2fe02d
EduardoDoradea <00086224@uca.edu.sv>
2025-09-25 21:56:29 -0600

    feat: Agregando comentarios al codigo

--- 00086224_practica4_sección1/Ejercicio Final/script.js
+++ 00086224_practica4_sección1/Ejercicio Final/script.js
@@ -21,40 +21,34 @@ function semana() {
     // el lowercase lo que hace es que toma la cadena del input y transforma toda la cadena en minusculas
 
     switch (diaUsuario.toLowerCase()) {
-        case 'lunes':
+        case 'lunes': 
+            // se puede utilizar este codigo tambien para poder mostrar en el html el mensaje al usuario
             //alert("Debo de atender a un cliente en especifico. ");
             element.textContent = "Debo de atender a un cliente en especifico.";
             break;
         case 'martes':
-            //alert("Debo visitar la agencia afuera de la ciudad. ");
             element.textContent = "Debo visitar la agencia afuera de la ciudad. ";
             break;
         case 'miercoles':
-            //alert("Debo llevar a mi hija a balé. ");
             element.textContent = "Debo llevar a mi hija a balé. ";
             break;
         case 'jueves':
-            //alert("Debo priorizar las entregas de desarrollo. ");
             element.textContent = "Debo priorizar las entregas de desarrollo. ";
             break;
         case 'viernes':
-            //alert("Debo de atender problemas de forma remota. ");
             element.textContent = "Debo de atender problemas de forma remota. ";
             break;
         case 'sabado':
-            //alert("Debo hacer lo que quiera mi esposa. ");
             element.textContent = "Debo hacer lo que quiera mi esposa. ";
             break;
         case 'domingo':
-            //alert("Dia del señor. ");
             element.textContent = "Dia del señor. ";
             break;
         default:
-            //alert("Ingrese un dia correcto.");
             element.textContent = "Ingrese un dia correcto.";
             break;
     }
-    //cuando finaliza el switch, limpia el input
+    //cuando finaliza el switch, "limpia" el input se le coloca un valor vacio para que se vea limpio 
     document.getElementById('inputDia').value = '';
 }
 
